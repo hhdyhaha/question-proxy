1756364e88f5adf281cc55056b70bcd902fb915b
hzdgzz <1164549585@qq.com>
2024-08-20 21:51:45 +0800

    ```cfeat(cors): 配置CORS中间件以允许指定来源
    配置了CORS中间件
    ```

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse, Response
+from fastapi.middleware.cors import CORSMiddleware
 import requests
 
 app = FastAPI()
 
+# 配置CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://123.56.15.95:81","http://123.56.15.95:82"],  # 允许的前端域名和端口
+    allow_credentials=True,
+    allow_methods=["*"],  # 允许的请求方法
+    allow_headers=["*"],  # 允许的请求头
+)
+
 PROXY_URL = "https://dashscope.aliyuncs.com"
 BEARER_TOKEN = "sk-e7197f1582324f47b7f4fef9f88f533b"  # 替换为你的 Bearer Token
-
 
 @app.post("/{path:path}")
 async def proxy_post(path: str, request: Request):
@@ -26,7 +35,6 @@ async def proxy_post(path: str, request: Request):
 
     return JSONResponse(content=json_response, status_code=response.status_code)
 
-
 @app.get("/{path:path}")
 async def proxy_get(path: str, request: Request):
     headers = {
@@ -44,8 +52,6 @@ async def proxy_get(path: str, request: Request):
 
     return JSONResponse(content=json_response, status_code=response.status_code)
 
-
 if __name__ == "__main__":
     import uvicorn
-
     uvicorn.run(app, host="0.0.0.0", port=8000)
